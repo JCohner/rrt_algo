7b0beb0e0727833b1eb11ce6d29ec5aac8af7500
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-12 15:09:51 -0500

    made class structure and able to plot given number of points to graph

--- rrt.py
+++ rrt.py
@@ -2,19 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-x_array = np.arange(100)
-y_array = np.arange(100)
+class rrt():
+	def __init__(self):
+		fig = plt.figure()
+		plt.plot()
+		plt.xlim((0,100))
+		plt.ylim((0,100))
 
-def gen_random():
-	x_coord = random.sample(x_array,1)[0]
-	y_coord = random.sample(y_array,1)[0]
-	pick = (x_coord,y_coord)
-	return pick
+	def gen_random(self):
+		x_coord = 100 * np.random.rand()
+		y_coord = 100 * np.random.rand()
+		pick = (x_coord,y_coord)
+		return pick
 
-fig = plt.figure()
-plt.plot()
-plt.xlim((0,100))
-plt.ylim((0,100))
-point = gen_random()
-plt.scatter(point[0],point[1])
+	def plot_point(self, point):
+		plt.scatter(point[0],point[1])
+
+	def add_to_tree(self, point):
+		self.plot_point(point)
+		#return tree
+
+	def run(self, number_points):
+		for x in range(number_points):
+			point = self.gen_random()
+			self.add_to_tree(point)
+
+rrt = rrt()
+rrt.run(10)
 plt.show()
+
