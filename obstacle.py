c0543efcc0cda974891b04c3089eb5d640f6cf5c
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-14 20:26:01 -0500

    implemented collision detection alogorithim, iterates through all circle obsctacle objects, finds if closest point to newly generated line is within any of those circles and then examines if the endpoints of those lines lay within circle.this will miss the case if line spans entire circle, will only occur is smallest circle is smaller than unit step, can be improved upon by also checking midpoint of line

--- obstacle.py
+++ obstacle.py
@@ -25,4 +25,37 @@ class obstacle_manager():
 		pick = (x_coord,y_coord)
 		return pick
 
-	def collision_detect(self, vector):
+	def euc_dist(self, p1, p2):
+		dist = np.sqrt(np.square(p1[0] - p2[0]) + np.square(p1[1] - p2[1]))
+		return dist
+
+	def collision_detect(self, p1, p2, dist):
+		#iterate over all circles if any intersect with new vert, return true
+		#we are going to stick pretty close to notation that Paul Bourke uses in his discusion @ http://paulbourke.net/geometry/pointlineplane/
+		for x in range(len(self.circle_obstacles)):
+			p3 = self.circle_obstacles[x][0] #gets center point
+			radius = self.circle_obstacles[x][1]
+			print("Circle center at " + str(p3))
+
+			u = ((p3[0] - p1[0]) * (p2[0] - p1[0]) + (p3[1] - p1[1]) * (p2[1] - p1[1]))/np.square(dist)
+
+			x_nearest = p1[0] + u * (p2[0] - p1[0])
+			y_nearest = p1[1] + u * (p2[1] - p1[1])
+			nearest_point = (x_nearest, y_nearest)
+
+			shortest_dist_to_line = self.euc_dist(nearest_point, p3)
+
+			#if the nearest distance to the line is outside the circle, does not interect
+			if shortest_dist_to_line > radius:
+				continue #check next circle
+
+			#now if the shortest distance is within the circle, we need to check the end points
+			dist_to_p1 = self.euc_dist(p1, p3)
+			dist_to_p2 = self.euc_dist(p2, p3)
+
+			if dist_to_p1 > radius and dist_to_p2 > radius:
+				continue
+			else:
+				return true
+
+		return false
