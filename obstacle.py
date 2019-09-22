758b1f084f4e0f6454c752d63dc2d0d708765635
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-22 14:26:24 -0500

    found a bug in the distance I was passing to the collision detector, was passing distance to theoretical point and not distrance to new vertex, additionally imporved (correctly implemented) collision detection by referencing scalar u in calculating closest point. this scalar is able to tell us whether or not the closest point to an obstacle lives on the line segment or not which is a critical distinction, only took 5 hours to find!

--- obstacle.py
+++ obstacle.py
@@ -45,28 +45,49 @@ class obstacle_manager():
 		#iterate over all circles if any intersect with new vert, return true
 		#we are going to stick pretty close to notation that Paul Bourke uses in his discusion @ http://paulbourke.net/geometry/pointlineplane/
 		for x in range(len(self.circle_obstacles)):
+			
 			p3 = self.circle_obstacles[x][0] #gets center point
+			#pdb.set_trace()
 			radius = self.circle_obstacles[x][1]
 
 			u = ((p3[0] - p1[0]) * (p2[0] - p1[0]) + (p3[1] - p1[1]) * (p2[1] - p1[1]))/np.square(dist)
+			#u is already telling us if the point exists on the line segment or not, with abs(u) > 1 meaning that it does not
 
-			x_nearest = p1[0] + u * (p2[0] - p1[0])
+
+			x_nearest = p1[0] + u * (p2[0] - p1[0]) #1 + u seems to adjust correctly
 			y_nearest = p1[1] + u * (p2[1] - p1[1])
 			nearest_point = (x_nearest, y_nearest)
 
 			shortest_dist_to_line = self.euc_dist(nearest_point, p3)
-
 			#if the nearest distance to the line is outside the circle, does not interect
 			if shortest_dist_to_line > radius:
-				continue #check next circle
+				#if the shortest distance to the line lies outisde of the circle, it cannot intersect therefore we can check the next circle obstacle
+				continue 
 
-			#now if the shortest distance is within the circle, we need to check the end points
+			#shortest distance to line within the circle, we need to check which of three cases
+			#Case 1: one of the end points exists within the circle INTERSECT CASE
+			#Case 2: both points lay outside circle
+				#2a: shortest point to line exists within circle, but line segment does not intersect circle NO-INTERSECT CASE
+				#2b: shortest point to line exists within circle, and line segment intersects with circle -> this means that shortest point to circle exists on line segment INTERSECT CASE
+
 			dist_to_p1 = self.euc_dist(p1, p3)
 			dist_to_p2 = self.euc_dist(p2, p3)
 
+			# print("dist to p1: " + str(dist_to_p1))
+			# print("dist_to_p2: " + str(dist_to_p2))
+			# print("radius of current cicle: " + str(radius))
+
 			if dist_to_p1 > radius and dist_to_p2 > radius:
-				continue
+				#print("both points outside of circle, checking if closest point lies on line")
+				#check if p3 exists on line, else continue
+				if u > 1:
+					#this means that the shortest point to the circle exists on the line and is within the circle, therefore the line intersects with the circle
+					return True
+				else:
+					continue
 			else:
+				#one of the points is within the line
+				#print("one of the points exists within circle")
 				return True
 
 		return False
